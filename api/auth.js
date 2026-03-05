706b8e51886231f9516f3c81081012949fbf6fc2
FLAG-A-E <74737920+FLAG-A-E@users.noreply.github.com>
2026-03-05 23:39:59 +0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,19 +1,12 @@
-// /api/auth.js
 import ImageKit from "imagekit";
 
 export default function handler(req, res) {
-  // ترويسة CORS
-  res.setHeader("Access-Control-Allow-Origin", "*"); // السماح لكل الدومينات
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end(); // Preflight request
-  }
-
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Only GET allowed" });
-  }
+  if (req.method === "OPTIONS") return res.status(200).end();
+  if (req.method !== "GET") return res.status(405).json({ error: "Only GET allowed" });
 
   const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC,
@@ -21,8 +14,8 @@ export default function handler(req, res) {
     urlEndpoint: process.env.IMAGEKIT_URL
   });
 
-  const auth = imagekit.getAuthenticationParameters();
+  // الحصول على توقيع صالح للرفع
+  const authenticationParameters = imagekit.getAuthenticationParameters();
 
-  res.status(200).json(auth);
+  res.status(200).json(authenticationParameters);
 }
-
